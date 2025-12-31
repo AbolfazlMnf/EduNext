dfde5071b1d650d024ddfa5dffde441c3334fa00
reza <rezakazemi1381@yahoo.com>
2026-01-01 03:10:03 +0330

    d

--- src/components/Layouts/Layouts.tsx
+++ src/components/Layouts/Layouts.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Navbar from "../navbar/navbar";
 import Container from "../container/Container";
+import NavContainer from "../container/NavContainer";
 
 function Layouts({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="bg-[#eeee] ">
+      <NavContainer>
+        <Navbar />
+      </NavContainer>
+      {children}
+    </div>
+  );
 }
 
 export default Layouts;
